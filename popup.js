bb614ced60afed86747538c21f443add36439c97
ericlove02 <eric.love02@yahoo.com>
2023-01-24 19:38:54 -0600

    storage kind of

--- popup.js
+++ popup.js
@@ -4,8 +4,37 @@ pageRewrite("onboarding2", "onboarding3", "button2")
 pageRewrite("onboarding3", "api-test", "button3")
 pageRewrite("api-test", "info1", "button4")
 // pageRewrite("info1", "info2", "button5")
-document.getElementById("close-button1").addEventListener("click", () => { window.close(); return; })
+// document.getElementById("close-button1").addEventListener("click", () => { window.close(); return; })
 document.getElementById("close-button2").addEventListener("click", () => { window.close(); return; })
+
+
+document.getElementById("button4").addEventListener("click", () => {
+    console.log("saving...");
+    setStorage("selected", true);
+    return;
+});
+document.getElementById("close-button1").addEventListener("click", () => {
+    console.log("getting...");
+    getStorage("selected");
+    return;
+});
+
+
+function getStorage(key) {
+    chrome.storage.sync.get(key, function (dataStored) {
+        if (dataStored["selected"]) {
+            selectedvar = dataStored["selected"];
+            console.log(selectedvar);
+        }
+    });
+}
+
+function setStorage(key, value) {
+    chrome.storage.sync.set({ key: value }, function () {
+        console.log("Data was saved.");
+    });
+}
+
 
 function pageRewrite(curr, dest, butt) {
     document.getElementById(butt).addEventListener("click", () => {
